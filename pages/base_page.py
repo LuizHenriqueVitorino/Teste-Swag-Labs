b46688c91ccfa78f0ceba9d3813bf0594b7d02a7
LuizHenriqueVitorino <henriquefisicoquimico@gmail.com>
2023-05-13 07:12:38 -0300

    Criacao da estrutura de paginas e implementacao do arquivo base_page com a classe BasePage

---
+++ pages/base_page.py
@@ -0,0 +1,23 @@
+
+
+class BasePage():
+    def __init__(self, driver, base_url='https://www.saucedemo.com/'):
+        self.url = base_url
+        self.driver = driver
+
+    def encontrar_elemento(self, locator):
+        return self.driver.find_element(*locator)
+    
+    def abrir(self, url):
+        self.driver.get(url)
+
+    def get_titulo(self):
+        return self.driver.title
+    
+    def escrever(self, locator, str):
+        elemento = self.encontrar_elemento(locator)
+        elemento.send_keys(str)
+
+    def clicar(self, locator):
+        elemento = self.encontrar_elemento(locator)
+        elemento.click()
